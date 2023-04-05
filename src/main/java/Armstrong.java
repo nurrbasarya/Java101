cb80554e7c8b4b7394c72e6728869d4fbefe31db
Nur Basarya <basaryamerve@gmail.com>
2023-04-05 22:57:37 +0300

    First Commit

---
+++ src/main/java/Armstrong.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number, basValue, result =0;
+        System.out.print("Sayıyı Giriniz: ");
+        number = input.nextInt();
+        while (number!=0){
+            basValue=number%10;
+            result+=basValue;
+            number /=10;
+        }
+        System.out.print(result);
+    }
+
+}
