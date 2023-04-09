234b4194536c9dd84411526cd4957a5d3d53cfc3
Nur Basarya <basaryamerve@gmail.com>
2023-04-09 22:22:08 +0300

    First Commit

---
+++ src/main/java/Palindrom.java
@@ -0,0 +1,21 @@
+
+public class Palindrom {
+
+    static boolean isPalindrom(int number){
+        int temp=number,reserveNumber=0,lastNumber;
+        while(temp!=0) {
+            lastNumber = temp % 10;
+            reserveNumber = (reserveNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if(number==reserveNumber)
+            return true;
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(353));
+    }
+}
